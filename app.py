58ff8fc10173f9892c742b9370a202775f01db67
imdaisylee <114948335+imdaisylee@users.noreply.github.com>
2024-05-30 19:55:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app1.run(debug=True, host='0.0.0.0', port=port)
