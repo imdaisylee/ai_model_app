c00efcac037a3af441c6f15e746131ef649a2d94
Vickie Knight <vickiek@uw.edu>
2024-05-30 23:54:29 -0700

    modify port

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8080))
     app1.run(debug=True, host='0.0.0.0', port=port)
